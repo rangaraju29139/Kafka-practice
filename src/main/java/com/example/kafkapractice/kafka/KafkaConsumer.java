d61987c16017d53dbce1c7b9c460510b9f00bb67
rangaraju penmetsa <rangaraju29139@gmail.com>
2024-01-03 21:05:35 +0530

    added the kafka listner to consume the message from the producer

---
+++ src/main/java/com/example/kafkapractice/kafka/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.example.kafkapractice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "sampleTopic", groupId = "myGroup")
+    public void consume(String message){
+
+        LOGGER.info("Message Received: " + message);
+
+
+
+    }
+}
